2213e1c7e9a3ffd7b0b76af8d661342607e92887
USER-ACER-PC <nicothecruzguardian@gmail.com>
2017-05-14 21:10:10 +0700

    Initial Commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl219/harest/fragment/SavedFragment.java
@@ -0,0 +1,30 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl219.harest.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import id.sch.smktelkom_mlg.privateassignment.xirpl219.harest.R;
+
+/**
+ * Created by USER-ACER-PC on 14/05/2017.
+ */
+
+public class SavedFragment extends Fragment {
+
+
+    public SavedFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_saved, container, false);
+    }
+
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl219/harest/model/SourcesResponse.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl219.harest.model;
+
+import java.util.List;
+
+/**
+ * Created by USER-ACER-PC on 14/05/2017.
+ */
+
+public class SourcesResponse {
+    //String url = "https://api.themoviedb.org/3/movie/top_rated?api_key=64369d17c75c1f4c8deb1e4aed649305&language=en-US&page=1";
+    public String page;
+    public List<Source> results;
+}
